a341ea8c6e5939d32988fcb40eef8c7f713d324b
Viktor Sysmäläinen <95356924+VSysmalainen@users.noreply.github.com>
2024-03-10 16:06:34 +0300

    Update rename_and_send_file.py
    test -> main env

--- rename_and_send_file.py
+++ rename_and_send_file.py
@@ -7,7 +7,7 @@ from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 
 from report_download import download_file
-from env_test import *
+from main_env import *
 
 
 files_dir = os.path.dirname(__file__) + '\\files'
