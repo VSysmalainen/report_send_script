19d1cb8605667018c6dd16438bd9af2b39d69ca2
VSysmalainen <sysmalainenviktor@gmail.com>
2024-03-10 14:22:36 +0300

    refactored selenium

--- main_env.py
+++ main_env.py
@@ -1,11 +1,9 @@
 import os
 
-
+browser = os.getenv('BROWSER', default='Edge')
+report_url = os.getenv('REPORT_URL', default='report url')
 login = os.getenv('RZD_USER', default='login')
 password = os.getenv('RZD_PASSWORD', default='password')
-browser = os.getenv('BROWSER', default='Edge')
-main_url = os.getenv('MAIN_URL', default='main url')
-second_url = os.getenv('SECOND_URL', default='second url')
 
 sender_email = os.getenv('SENDER_EMAIL', default='sender email')
 receiver_email = os.getenv('RECEIVER_EMAIL', default='receiver email')
