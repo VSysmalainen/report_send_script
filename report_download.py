91b934bed1fbd9da2d3323b30b842cf68cbc57c3
Viktor Sysmäläinen <95356924+VSysmalainen@users.noreply.github.com>
2024-03-10 16:38:48 +0300

    Update report_download.py
    less comments

--- report_download.py
+++ report_download.py
@@ -33,28 +33,16 @@ def download_file(browser, files_dir, report_url, login, password):
 
 
     driver.maximize_window()
-
     driver.get(report_url)
 
-    # Вводим логин
     driver.find_element(By.XPATH, '//*[@id="j_username"]').send_keys(login)
-
-    # Вводим пароль
     driver.find_element(By.XPATH, '//*[@id="j_password_pseudo"]').send_keys(password)
 
-    # Ожидание кликабельности кнопки Login и клик по ней
     WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="submitButton"]'))).click()
-
-    # Ожидание кликабельности кнопки Apply и клик по ней
     WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="apply"]'))).click()
-
-    # Ожидание кликабельности кнопки Ok и клик по ней
     WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="ok"]'))).click()
-
-    # Ожидание кликабельности кнопки Menu и клик по ней
     WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="export"]'))).click()
 
-    # Находим в выпадающем списке Menu нужный элемент и кликаем по нему, скачивается файл
     driver.find_elements(By.CSS_SELECTOR, "[id*=menuList_simpleAction]")[0].click()
     time.sleep(2)
 
